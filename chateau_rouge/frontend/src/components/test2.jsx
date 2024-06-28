ea2ccc7b8abdfd61771e57384e6a88afd8612b24
abdelkader <abdelkaderboukart@gmail.com>
2024-06-28 04:09:29 +0100

    "Updated Test2 component with changes to imports, styles, and added FlipLink component with animations"

--- chateau_rouge/frontend/src/components/test2.jsx
+++ chateau_rouge/frontend/src/components/test2.jsx
@@ -1,8 +1,9 @@
-import { useContext, useState, useEffect } from "react";
+import  React, { useContext, useState, useEffect } from "react";
 import AuthContex from "../context/AuthContext";
 import axios from "axios";
 import Select from "react-select";
 import { NavBar } from "./navbar";
+import { motion } from "framer-motion";
 
 const Test2 = () => {
   const [selectedCompany, setSelectedCompany] = useState({
@@ -48,18 +49,29 @@ const Test2 = () => {
   }, [errors]);
   return (
     <div
-      className="flex justify-center items-center bg-black"
+      className=" bg-black"
+      // flex justify-center items-center
       style={{ height: "100vh" }}
     >
+      <NavBar />
+
       <form
         action="post"
-        className="flex justify-center items-center bg-black"
+        className="flex justify-center w-full items-center bg-black"
         onSubmit={handleSubmit}
       >
-        <div className=" relative h-96 min-w-[277px] w-[40%] bg-white rounded-[15px]">
+        <FlipLink>createnewclient</FlipLink>
+        <div className=" relative h-96 min-w-[277px] w-[40%] bg-[#effef7] rounded-[15px]">
+          {/* #fafaf9 */}
           <div
-            style={{ display: "flex", flexDirection: "column", flexGrow: 1, padding:10 }}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              flexGrow: 1,
+              padding: 10,
+            }}
           >
+            <label htmlFor="name">name:</label>
             <input
               type="text"
               name="name"
@@ -67,11 +79,14 @@ const Test2 = () => {
               style={{
                 margin: "10px 0 10px 0",
                 height: 50,
+                backgroundColor: "#effef7",
+                // #fafaf9
               }}
               onChange={(e) => {
                 setClient({ ...client, [e.target.name]: e.target.value });
               }}
             />
+            <label htmlFor="prename">prename:</label>
             <input
               type="text"
               name="prename"
@@ -79,22 +94,29 @@ const Test2 = () => {
               style={{
                 margin: "10px 0 10px 0",
                 height: 50,
+                backgroundColor: "#effef7",
+                // #fafaf9
               }}
               onChange={(e) => {
                 setClient({ ...client, [e.target.name]: e.target.value });
               }}
             />
+            <label htmlFor="">company:</label>
             <Select
               styles={{
                 control: (styles) => ({
                   ...styles,
                   height: 40,
                   width: "100%",
+                  backgroundColor: "#effef7",
+                  // #fafaf9
                 }),
                 menu: (styles) => ({
                   ...styles,
                   width: "100%",
                   maxHeight: "90vh",
+                  backgroundColor: "#effef7",
+                  // #fafaf9
                 }),
               }}
               value={selectedCompany ? selectedCompany.id : ""}
@@ -114,7 +136,10 @@ const Test2 = () => {
               placeholder="Select a company"
             />
             {selectedCompany && (
-              <div>Selected company: {selectedCompany.name}</div>
+              <div className="absolute flex justify-center items-center left-[40%] bottom-[0px] h-14 w-[60%] p-3">
+                {" "}
+                <h1> Selected company: {selectedCompany.name}</h1>
+              </div>
             )}
           </div>
           <div
@@ -138,7 +163,7 @@ const Test2 = () => {
           ></div>
           <div className=" absolute bottom-0 flex justify-center items-center bg-black w-[40%] h-14 rounded-tr-[15px]">
             <button
-              className="bg-blue-800 text-white rounded-[10px]"
+              className="bg-[#325d4b] text-white rounded-[10px]"
               style={{ height: 41, width: "calc( 100% - 15px )" }}
               type="submit"
             >
@@ -152,3 +177,69 @@ const Test2 = () => {
 };
 
 export default Test2;
+
+
+const DURATION = 0.25;
+const STAGGER = 0.025;
+
+const FlipLink = ({ children}) => {
+  return (
+    <motion.div
+      initial="initial"
+      whileHover="hovered"
+      className="relative block overflow-hidden whitespace-nowrap text-4xl font-black uppercase sm:text-7xl md:text-8xl lg:text-9xl m-4"
+      style={{
+        lineHeight: 0.75,
+      }}
+    >
+      <div className="">
+        {children.split("").map((l, i) => (
+          <motion.span
+            variants={{
+              initial: {
+                y: 0,
+              },
+              hovered: {
+                y: "-110%",
+              },
+            }}
+            transition={{
+              duration: DURATION,
+              ease: "easeInOut",
+              delay: STAGGER * i,
+            }}
+            className="inline-block"
+            key={i}
+          >
+            {l}
+          </motion.span>
+        ))}
+      </div>
+      <div className="absolute inset-0">
+        {children.split("").map((l, i) => (
+          <motion.span
+            variants={{
+              initial: {
+                y: "110%",
+                filter: "drop-shadow(0px 0px 0px rgba(255, 255, 255, 0))",
+              },
+              hovered: {
+                y: 0,
+                filter: "drop-shadow(0px 5px 7px rgba(255, 255, 255, 0.5))",
+              },
+            }}
+            transition={{
+              duration: DURATION,
+              ease: "easeInOut",
+              delay: STAGGER * i,
+            }}
+            className="inline-block  text-[#effef7]"
+            key={i}
+          >
+            {l}
+          </motion.span>
+        ))}
+      </div>
+    </motion.div>
+  );
+};
